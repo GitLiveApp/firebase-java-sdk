5859a108ac5146dfc44c58e730c2b640c539c6bb
nbransby <nbransby@gmail.com>
2024-02-25 00:17:41 +0800

    fix crash after update to firebase functions 20.4.0

--- src/main/java/android/content/Context.kt
+++ src/main/java/android/content/Context.kt
@@ -58,7 +58,7 @@ open class Context {
             override fun getString(key: String, defaultValue: String?): String? {
                 return when {
                     key == "last-used-date" -> FirebasePlatform.firebasePlatform.retrieve(key) ?: defaultValue
-                    key.startsWith("|T|")  -> null
+                    key.contains("|T|")  -> null
                     key.startsWith("com.google.firebase.auth.FIREBASE_USER") ->
                         FirebasePlatform.firebasePlatform.retrieve(key) ?: defaultValue
                     else -> throw IllegalArgumentException(key)
