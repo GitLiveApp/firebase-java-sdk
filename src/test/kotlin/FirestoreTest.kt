a96a90190c2d348ef87c32cc26ffdc72e9463b9c
František Maša <frantisekmasa1@gmail.com>
2024-04-22 20:30:27 +0200

    Destroy all Firebase instances before each tests

--- src/test/kotlin/FirestoreTest.kt
+++ src/test/kotlin/FirestoreTest.kt
@@ -11,7 +11,7 @@ import org.junit.Before
 import org.junit.Test
 import java.io.File
 
-class FirestoreTest {
+class FirestoreTest : FirebaseTest() {
     @Before
     fun initialize() {
         FirebasePlatform.initializeFirebasePlatform(object : FirebasePlatform() {
