c811e394066cad9ab1d503237112bd7dacff6c8c
Jakub Kostka <jacob.cube.mexx@gmail.com>
2024-11-07 16:11:35 +0100

    ktlint formatting + update of user data after signInWithCustomToken + photoUrl and displayName implementation

--- src/main/java/com/google/firebase/auth/FirebaseUser.kt
+++ src/main/java/com/google/firebase/auth/FirebaseUser.kt
@@ -5,28 +5,45 @@ import com.google.android.gms.tasks.Task
 abstract class FirebaseUser {
     abstract val uid: String
     abstract val email: String?
+    abstract val photoUrl: String?
+    abstract val displayName: String?
     abstract val isAnonymous: Boolean
+
     abstract fun delete(): Task<Void>
+
     abstract fun reload(): Task<Void>
-    abstract fun verifyBeforeUpdateEmail(newEmail: String, actionCodeSettings: ActionCodeSettings?): Task<Unit>
+
+    abstract fun verifyBeforeUpdateEmail(
+        newEmail: String,
+        actionCodeSettings: ActionCodeSettings?,
+    ): Task<Unit>
+
     abstract fun updateEmail(email: String): Task<Unit>
 
-    val displayName: String get() = TODO()
+    abstract fun getIdToken(forceRefresh: Boolean): Task<GetTokenResult>
+
+    abstract fun updateProfile(request: UserProfileChangeRequest): Task<Unit>
+
     val phoneNumber: String get() = TODO()
-    val photoUrl: String? get() = TODO()
     val isEmailVerified: Boolean get() = TODO()
     val metadata: FirebaseUserMetadata get() = TODO()
     val multiFactor: MultiFactor get() = TODO()
     val providerData: List<UserInfo> get() = TODO()
     val providerId: String get() = TODO()
-    abstract fun getIdToken(forceRefresh: Boolean): Task<GetTokenResult>
+
     fun linkWithCredential(credential: AuthCredential): Task<AuthResult> = TODO()
+
     fun sendEmailVerification(): Task<Unit> = TODO()
+
     fun sendEmailVerification(actionCodeSettings: ActionCodeSettings): Task<Unit> = TODO()
+
     fun unlink(provider: String): Task<AuthResult> = TODO()
+
     fun updatePassword(password: String): Task<Unit> = TODO()
+
     fun updatePhoneNumber(credential: AuthCredential): Task<Unit> = TODO()
-    fun updateProfile(request: UserProfileChangeRequest): Task<Unit> = TODO()
+
     fun reauthenticate(credential: AuthCredential): Task<Unit> = TODO()
+
     fun reauthenticateAndRetrieveData(credential: AuthCredential): Task<AuthResult> = TODO()
 }
--- src/test/kotlin/AuthTest.kt
+++ src/test/kotlin/AuthTest.kt
@@ -1,47 +1,97 @@
-import com.google.firebase.auth.FirebaseAuth
+import android.net.Uri
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertThrows
+import org.junit.Before
 import org.junit.Test
+import java.util.UUID
 
 class AuthTest : FirebaseTest() {
-    private fun createAuth(): FirebaseAuth {
-        return FirebaseAuth(app).apply {
+    private val email = "email${UUID.randomUUID()}@example.com"
+
+    @Before
+    fun initialize() {
+        auth.apply {
             useEmulator("localhost", 9099)
         }
     }
 
     @Test
-    fun `should authenticate via anonymous auth`() = runTest {
-        val auth = createAuth()
+    fun `should authenticate via anonymous auth`() =
+        runTest {
+            auth.signInAnonymously().await()
 
-        auth.signInAnonymously().await()
-
-        assertEquals(true, auth.currentUser?.isAnonymous)
-    }
-
-    @Test
-    fun `should authenticate via email and password`() = runTest {
-        val auth = createAuth()
-
-        auth.signInWithEmailAndPassword("email@example.com", "securepassword").await()
-
-        assertEquals(false, auth.currentUser?.isAnonymous)
-    }
-
-    @Test
-    fun `should throw exception on invalid password`() {
-        val auth = createAuth()
-
-        val exception = assertThrows(FirebaseAuthInvalidUserException::class.java) {
-            runBlocking {
-                auth.signInWithEmailAndPassword("email@example.com", "wrongpassword").await()
-            }
+            assertEquals(true, auth.currentUser?.isAnonymous)
         }
 
-        assertEquals("INVALID_PASSWORD", exception.errorCode)
-    }
+    @Test
+    fun `should create user via email and password`() =
+        runTest {
+            val createResult = auth.createUserWithEmailAndPassword(email, "test123").await()
+            assertNotEquals(null, createResult.user?.uid)
+            assertEquals(null, createResult.user?.displayName)
+            // assertEquals(null, createResult.user?.phoneNumber)
+            assertEquals(false, createResult.user?.isAnonymous)
+            assertEquals(email, createResult.user?.email)
+            assertNotEquals("", createResult.user!!.email)
+
+            val signInResult = auth.signInWithEmailAndPassword(email, "test123").await()
+            assertEquals(createResult.user?.uid, signInResult.user?.uid)
+        }
+
+    @Test
+    fun `should authenticate via email and password`() =
+        runTest {
+            auth.createUserWithEmailAndPassword(email, "test123").await()
+
+            auth.signInWithEmailAndPassword(email, "test123").await()
+
+            assertEquals(false, auth.currentUser?.isAnonymous)
+        }
+
+    @Test
+    fun `should update displayName and photoUrl`() =
+        runTest {
+            auth
+                .createUserWithEmailAndPassword(email, "test123")
+                .await()
+                .user
+            auth.currentUser
+                ?.updateProfile(
+                    com.google.firebase.auth.UserProfileChangeRequest
+                        .Builder()
+                        .setDisplayName("testDisplayName")
+                        .setPhotoUri(Uri.parse("https://picsum.photos/100"))
+                        .build(),
+                )?.await()
+            assertEquals("testDisplayName", auth.currentUser?.displayName)
+            assertEquals("https://picsum.photos/100", auth.currentUser?.photoUrl)
+        }
+
+    @Test
+    fun `should sign in anonymously`() =
+        runTest {
+            val signInResult = auth.signInAnonymously().await()
+            assertNotEquals("", signInResult.user!!.email)
+            assertEquals(true, signInResult.user?.isAnonymous)
+        }
+
+    @Test
+    fun `should throw exception on invalid password`() =
+        runTest {
+            auth.createUserWithEmailAndPassword(email, "test123").await()
+
+            val exception =
+                assertThrows(FirebaseAuthInvalidUserException::class.java) {
+                    runBlocking {
+                        auth.signInWithEmailAndPassword(email, "wrongpassword").await()
+                    }
+                }
+
+            assertEquals("INVALID_PASSWORD", exception.errorCode)
+        }
 }
