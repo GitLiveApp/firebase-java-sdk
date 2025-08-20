20354122efafeeeb6623bb8c3ef74b479a7318c0
nbransby <nbransby@gmail.com>
2025-08-20 11:16:14 +0100

    avoid use of java.lang.management.ManagementFactory

--- src/main/java/android/os/SystemClock.java
+++ src/main/java/android/os/SystemClock.java
@@ -1,12 +1,14 @@
 package android.os;
 
-import java.lang.management.ManagementFactory;
 
 public final class SystemClock {
+
+    private static final long startTime = System.currentTimeMillis();
+
     public static long elapsedRealtime() {
-        return ManagementFactory.getRuntimeMXBean().getUptime();
+        return System.currentTimeMillis() - startTime;
     }
     public static long uptimeMillis() {
-        return ManagementFactory.getRuntimeMXBean().getUptime();
+        return System.currentTimeMillis() - startTime;
     }
 }
